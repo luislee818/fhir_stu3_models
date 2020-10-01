3ab7f4ac406ae046759e30bd0c6fd4e75a438113
Dapeng Li <luislee818@gmail.com>
2020-10-01 08:39:50 -0400

    feature: add Practitioner#practitionerRole from v1.4.0

--- lib/fhir_stu3_models/fhir/resources/Practitioner.rb
+++ lib/fhir_stu3_models/fhir/resources/Practitioner.rb
@@ -24,7 +24,8 @@ module FHIR
         'birthDate' => {'type'=>'date', 'path'=>'Practitioner.birthDate', 'min'=>0, 'max'=>1},
         'photo' => {'type'=>'Attachment', 'path'=>'Practitioner.photo', 'min'=>0, 'max'=>Float::INFINITY},
         'qualification' => {'type'=>'Practitioner::Qualification', 'path'=>'Practitioner.qualification', 'min'=>0, 'max'=>Float::INFINITY},
-        'communication' => {'valid_codes'=>{'urn:ietf:bcp:47'=>['ar', 'bn', 'cs', 'da', 'de', 'de-AT', 'de-CH', 'de-DE', 'el', 'en', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-NZ', 'en-SG', 'en-US', 'es', 'es-AR', 'es-ES', 'es-UY', 'fi', 'fr', 'fr-BE', 'fr-CH', 'fr-FR', 'fy', 'fy-NL', 'hi', 'hr', 'it', 'it-CH', 'it-IT', 'ja', 'ko', 'nl', 'nl-BE', 'nl-NL', 'no', 'no-NO', 'pa', 'pt', 'pt-BR', 'ru', 'ru-RU', 'sr', 'sr-SP', 'sv', 'sv-SE', 'te', 'zh', 'zh-CN', 'zh-HK', 'zh-SG', 'zh-TW']}, 'type'=>'CodeableConcept', 'path'=>'Practitioner.communication', 'min'=>0, 'max'=>Float::INFINITY, 'binding'=>{'strength'=>'extensible', 'uri'=>'http://hl7.org/fhir/ValueSet/languages'}}
+        'communication' => {'valid_codes'=>{'urn:ietf:bcp:47'=>['ar', 'bn', 'cs', 'da', 'de', 'de-AT', 'de-CH', 'de-DE', 'el', 'en', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-NZ', 'en-SG', 'en-US', 'es', 'es-AR', 'es-ES', 'es-UY', 'fi', 'fr', 'fr-BE', 'fr-CH', 'fr-FR', 'fy', 'fy-NL', 'hi', 'hr', 'it', 'it-CH', 'it-IT', 'ja', 'ko', 'nl', 'nl-BE', 'nl-NL', 'no', 'no-NO', 'pa', 'pt', 'pt-BR', 'ru', 'ru-RU', 'sr', 'sr-SP', 'sv', 'sv-SE', 'te', 'zh', 'zh-CN', 'zh-HK', 'zh-SG', 'zh-TW']}, 'type'=>'CodeableConcept', 'path'=>'Practitioner.communication', 'min'=>0, 'max'=>Float::INFINITY, 'binding'=>{'strength'=>'extensible', 'uri'=>'http://hl7.org/fhir/ValueSet/languages'}},
+        'practitionerRole' => {'type'=>'Practitioner::Practitionerrole', 'path'=>'Practitioner.practitionerRole', 'min'=>0, 'max'=>Float::INFINITY}
       }
 
       class Qualification < FHIR::STU3::Model
@@ -51,6 +52,38 @@ module FHIR
         attr_accessor :issuer            # 0-1 Reference(Organization)
       end
 
+      class Practitionerrole < FHIR::STU3::Model
+        include FHIR::STU3::Hashable
+        include FHIR::STU3::Json
+        include FHIR::STU3::Xml
+
+        METADATA = {
+          'id' => {'type'=>'id', 'path'=>'Practitionerrole.id', 'min'=>0, 'max'=>1},
+          'extension' => {'type'=>'Extension', 'path'=>'Practitionerrole.extension', 'min'=>0, 'max'=>Float::INFINITY},
+          'modifierExtension' => {'type'=>'Extension', 'path'=>'Practitionerrole.modifierExtension', 'min'=>0, 'max'=>Float::INFINITY},
+          'organization' => {'type_profiles'=>['http://hl7.org/fhir/StructureDefinition/Organization'], 'type'=>'Reference', 'path'=>'Practitionerrole.organization', 'min'=>0, 'max'=>1},
+          'role' => {'valid_codes'=>{'http://hl7.org/fhir/practitioner-role'=>['doctor', 'nurse', 'pharmacist', 'researcher', 'teacher', 'ict']}, 'type'=>'CodeableConcept', 'path'=>'Practitionerrole.role', 'min'=>0, 'max'=>1, 'binding'=>{'strength'=>'example', 'uri'=>'http://hl7.org/fhir/ValueSet/practitioner-role'}},
+          'specialty' => {'valid_codes'=>{'http://hl7.org/fhir/practitioner-specialty'=>['cardio', 'dent', 'dietary', 'midw', 'sysarch']}, 'type'=>'CodeableConcept', 'path'=>'Practitionerrole.specialty', 'min'=>0, 'max'=>Float::INFINITY, 'binding'=>{'strength'=>'example', 'uri'=>'http://hl7.org/fhir/ValueSet/practitioner-specialty'}},
+          'identifier' => {'type'=>'Identifier', 'path'=>'Practitionerrole.identifier', 'min'=>0, 'max'=>Float::INFINITY},
+          'telecom' => {'type'=>'ContactPoint', 'path'=>'Practitionerrole.telecom', 'min'=>0, 'max'=>Float::INFINITY},
+          'period' => {'type'=>'Period', 'path'=>'Practitionerrole.period', 'min'=>0, 'max'=>1},
+          'location' => {'type_profiles'=>['http://hl7.org/fhir/StructureDefinition/Location'], 'type'=>'Reference', 'path'=>'Practitionerrole.location', 'min'=>0, 'max'=>Float::INFINITY},
+          'healthcareService' => {'type_profiles'=>['http://hl7.org/fhir/StructureDefinition/HealthcareService'], 'type'=>'Reference', 'path'=>'Practitionerrole.healthcareService', 'min'=>0, 'max'=>Float::INFINITY}
+        }
+
+        attr_accessor :id                # 0-1 id
+        attr_accessor :extension         # 0-* [ Extension ]
+        attr_accessor :modifierExtension # 0-* [ Extension ]
+        attr_accessor :organization      # 0-1 Reference(Organization)
+        attr_accessor :role              # 0-1 CodeableConcept
+        attr_accessor :specialty         # 0-* [ CodeableConcept ]
+        attr_accessor :identifier        # 0-* [ Identifier ]
+        attr_accessor :telecom           # 0-* [ ContactPoint ]
+        attr_accessor :period            # 0-1 Period
+        attr_accessor :location          # 0-* [ Reference(Location) ]
+        attr_accessor :healthcareService # 0-* [ Reference(HealthcareService) ]
+      end
+
       attr_accessor :id                # 0-1 id
       attr_accessor :meta              # 0-1 Meta
       attr_accessor :implicitRules     # 0-1 uri
@@ -69,6 +102,7 @@ module FHIR
       attr_accessor :photo             # 0-* [ Attachment ]
       attr_accessor :qualification     # 0-* [ Practitioner::Qualification ]
       attr_accessor :communication     # 0-* [ CodeableConcept ]
+      attr_accessor :practitionerRole  # 0-* [ Practitioner::Practitionerrole ]
 
       def resourceType
         'Practitioner'
